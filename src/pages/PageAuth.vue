2c9ed9ab3f849337263e290b607b87c4dec8360c
Danny Connell <sendstufftodanny@gmail.com>
2024-12-11 05:26:01 +0000

    6.4

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -16,6 +16,35 @@
           <q-tab name="register" label="Register" />
         </q-tabs>
       </q-card-section>
+
+      <q-card-section>
+        <q-form>
+          <q-input
+            class="q-mb-md"
+            bg-color="white"
+            label="Email"
+            type="email"
+            autocomplete="email"
+            filled
+          />
+          <q-input
+            class="q-mb-md"
+            bg-color="white"
+            label="Password"
+            type="password"
+            autocomplete="current-password"
+            filled
+          />
+          <q-btn
+            to="/"
+            class="full-width"
+            color="white"
+            label="Submit"
+            outline
+            no-caps
+          />
+        </q-form>
+      </q-card-section>
     </q-card>
   </q-page>
 </template>
