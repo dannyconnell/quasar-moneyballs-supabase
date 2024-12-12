1db8ad29719cd32e6afa06a7f7b93952f9262651
Danny Connell <sendstufftodanny@gmail.com>
2024-12-12 10:44:00 +0000

    9.2

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,8 +1,15 @@
 import { boot } from 'quasar/wrappers'
+import { useStoreAuth } from 'src/stores/storeAuth'
 
 export default boot(({ router }) => {
   router.beforeEach((to, from) => {
-    console.log('from: ', from)
-    console.log('to: ', to)
+    const storeAuth = useStoreAuth()
+
+    if (!storeAuth.userDetails.id && to.path !== '/auth') {
+      return '/auth'
+    }
+    if (storeAuth.userDetails.id && to.path === '/auth') {
+      return false
+    }
   })
 })
