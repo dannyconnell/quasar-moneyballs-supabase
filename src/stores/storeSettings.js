98b8682c2e07000303a010e605ef17df57344ca1
Danny Connell <sendstufftodanny@gmail.com>
2024-12-17 07:56:01 +0000

    12.6

--- src/stores/storeSettings.js
+++ src/stores/storeSettings.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import { reactive, watch } from 'vue'
 import { Dark, LocalStorage } from 'quasar'
+import { useStoreAuth } from 'src/stores/storeAuth'
+import { useShowErrorMessage } from 'src/use/useShowErrorMessage'
 import supabase from 'src/config/supabase'
 
 export const useStoreSettings = defineStore('settings', () => {
@@ -52,7 +54,19 @@ export const useStoreSettings = defineStore('settings', () => {
     }
 
     const uploadAvatar = async file => {
-      console.log('upload file:', file)
+
+      const storeAuth = useStoreAuth(),
+            folderPath = storeAuth.userDetails.id,
+            fileName = `${ Date.now() }_${ file.name.replaceAll(' ', '_')}`
+
+      const { data, error } = await supabase
+        .storage
+        .from('avatars')
+        .upload(`${ folderPath }/${ fileName }`, file)
+
+      if (error) useShowErrorMessage(error.message)
+      if (data) console.log(data)
+      
     }
 
 
