dc014b98cfd176326392d4451bf6d05cc0fbdd19
Danny Connell <sendstufftodanny@gmail.com>
2024-12-21 08:35:07 +0000

    14.7

--- supabase/functions/greeting/index.ts
+++ supabase/functions/greeting/index.ts
@@ -4,6 +4,7 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log("Hello from Functions!")
@@ -15,6 +16,13 @@ Deno.serve(async (req) => {
   }
 
   try {
+
+    const supabase = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
     const greetings = ['Hello!', 'Welcome!', 'Word to your moms!', 'Word!', 'Peace & Love!']
     const greeting = greetings[Math.floor(Math.random() * greetings.length)]
 
